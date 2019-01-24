e3d0d5e92645195caa61c95cd7f22261f17523ba
Khetag Tebiev <khetag_tebiev@epam.com>
2019-01-25 02:58:07 +0300

    Add for all three pages inheritance from base.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+site_menu_links = [
+    {'path_name': 'index', 'name': 'домой'},
+    {'path_name': 'products', 'name': 'продукты'},
+    {'path_name': 'contact', 'name': 'контакты'}
+]
+
 
 def index(request: HttpRequest):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'site_menu_links': site_menu_links
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request: HttpRequest):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'Каталог',
+        'site_menu_links': site_menu_links
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request: HttpRequest):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'Контакты',
+        'site_menu_links': site_menu_links
+    }
+    return render(request, 'mainapp/contact.html', context)
