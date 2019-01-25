2a623a7d10185fbaad6ff16964e3d0e23ba21fe9
Khetag Tebiev <khetag_tebiev@epam.com>
2019-01-25 05:20:27 +0300

    Improve in products page related products section and product type menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -15,10 +15,40 @@ def index(request: HttpRequest):
     return render(request, 'mainapp/index.html', context)
 
 
-def products(request: HttpRequest):
+def products(request: HttpRequest, current_product_type='all'):
+
+    product_type_menu_links = [
+        {'url_param': 'all', 'name': 'все'},
+        {'url_param': 'home', 'name': 'дом'},
+        {'url_param': 'office', 'name': 'офис'},
+        {'url_param': 'modern', 'name': 'модерн'},
+        {'url_param': 'classic', 'name': 'классика'}
+    ]
+
+    related_products = [
+        {
+            'id': 11,
+            'description_title': 'Стул повышенного качества',
+            'description_text': 'Не оторваться.'
+        },
+        {
+            'id': 21,
+            'description_title': 'Стул повышенного качества',
+            'description_text': 'Не оторваться.'
+        },
+        {
+            'id': 31,
+            'description_title': 'Стул повышенного качества',
+            'description_text': 'Не оторваться.'
+        },
+    ]
+
     context = {
         'title': 'Каталог',
-        'site_menu_links': site_menu_links
+        'site_menu_links': site_menu_links,
+        'product_type_menu_links': product_type_menu_links,
+        'current_product_type': current_product_type,
+        'related_products': related_products
     }
     return render(request, 'mainapp/products.html', context)
 
