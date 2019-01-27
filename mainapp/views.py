c0bda626869172ae17c2bba2af45dc210cfd689d
Khetag Tebiev <khetag_tebiev@epam.com>
2019-01-27 19:26:04 +0300

    Apply the site navigation template on all pages

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,14 +3,14 @@ from django.http import HttpRequest
 
 from my_utils import get_data_from_json
 
-site_menu_links = get_data_from_json('site_menu_links.json')
+site_navigation_links = get_data_from_json('site_navigation_links.json')
 product_type_menu_links = get_data_from_json('product_type_menu_links.json')
 related_products = get_data_from_json('related_products.json')
 
 
 def index(request: HttpRequest):
     context = {
-        'site_menu_links': site_menu_links
+        'site_navigation_links': site_navigation_links
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -18,7 +18,7 @@ def index(request: HttpRequest):
 def products(request: HttpRequest, current_product_type='all'):
     context = {
         'title': 'Каталог',
-        'site_menu_links': site_menu_links,
+        'site_navigation_links': site_navigation_links,
         'product_type_menu_links': product_type_menu_links,
         'current_product_type': current_product_type,
         'related_products': related_products
@@ -29,6 +29,6 @@ def products(request: HttpRequest, current_product_type='all'):
 def contact(request: HttpRequest):
     context = {
         'title': 'Контакты',
-        'site_menu_links': site_menu_links
+        'site_navigation_links': site_navigation_links
     }
     return render(request, 'mainapp/contact.html', context)
