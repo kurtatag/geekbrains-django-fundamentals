c95558006fd188dcb25f1dee589c11cd82ad7758
Khetag Tebiev <khetag_tebiev@epam.com>
2019-01-28 05:43:39 +0300

    Add loading products and categories from the database

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Product, ProductCategory
 
 from my_utils import get_data_from_json
 
@@ -16,12 +17,22 @@ def index(request: HttpRequest):
 
 
 def products(request: HttpRequest, current_product_category='all'):
+    categories = ProductCategory.objects
+    products = Product.objects
+
+    product_category_list = ['all'] + [c.name for c in categories.all()]
+
+    if current_product_category == 'all':
+        product_list = products.all()
+    else:
+        product_list = products.filter(category__name=current_product_category)
+
     context = {
         'title': 'products',
         'site_navigation_links': site_navigation_links,
-        'product_category_menu_links': product_category_menu_links,
+        'product_category_list': product_category_list,
         'current_product_category': current_product_category,
-        'product_list': product_list[current_product_category]
+        'product_list': product_list
     }
     return render(request, 'mainapp/products.html', context)
 
