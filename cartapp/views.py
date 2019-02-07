9f8786c5f3808e447b64b002b122de5034a64617
Khetag Tebiev <khetag_tebiev@epam.com>
2019-02-07 23:57:10 +0300

    Modify cart.html

--- cartapp/views.py
+++ cartapp/views.py
@@ -10,10 +10,22 @@ site_navigation_links = get_data_from_json('site_navigation_links.json')
 
 def cart(request: HttpRequest):
     cart = Cart.objects.filter(user=request.user)
+
+    # prepare cart info to be displayed on the site navigation menu
+    cart_info = {
+        'items_total': 0,
+        'price_total': 0
+    }
+
+    if request.user.is_authenticated:
+        cart_info['items_total'] = Cart.cart_items_total(user=request.user)
+        cart_info['price_total'] = Cart.cart_price_total(user=request.user)
+
     context = {
         'title': 'cart',
         'site_navigation_links': site_navigation_links,
-        'cart': cart
+        'cart': cart,
+        'cart_info': cart_info,
     }
     return render(request, 'cartapp/cart.html', context)
 
@@ -33,13 +45,8 @@ def cart_add(request: HttpRequest, pk: int):
 
 
 def cart_remove(request: HttpRequest, pk: int):
+
     cart_product = get_object_or_404(Cart, pk=pk)
-
-    cart_product.quantity -= 1
-
-    if cart_product.quantity < 1:
-        cart_product.delete()
-    else:
-        cart_product.save()
+    cart_product.delete()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
