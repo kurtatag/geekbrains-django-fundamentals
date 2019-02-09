b29ac44065ea11017a4b47cc990ecc0fb4db16c8
Khetag Tebiev <khetag_tebiev@epam.com>
2019-02-09 18:40:55 +0300

    Change static files location & congiguration

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ffhl3*jc19akyhxek!&!n8t%#1v=4nm5ou+web4p2%5^il)z-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'didinag.pythonanywhere.com']
 
 
 # Application definition
@@ -120,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "geekshop/static"),
 ]
 
 
