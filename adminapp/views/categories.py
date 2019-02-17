29a91e9bb5b99696470f8420b7018f84501eac67
Khetag Tebiev <khetag_tebiev@epam.com>
2019-02-17 13:21:04 +0300

    Fix bug: typo

--- adminapp/views/categories.py
+++ adminapp/views/categories.py
@@ -74,7 +74,7 @@ class CategoryUpdate(SuccessMessageMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'new category'
+        context['title'] = 'category: {}'.format(self.get_object().name)
         return context
 
     def get_success_url(self):
